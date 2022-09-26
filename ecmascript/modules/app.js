b4a23d7da8f70ff8063dc32f6768a359c0fe8002
suryatejakancharla <suryatejakancharla143@gmail.com>
2022-09-26 12:33:07 +0530

    javascript surya

---
+++ ecmascript/modules/app.js
@@ -0,0 +1,5 @@
+import { greet, PI, multiplyNumbers } from './main.js';
+
+alert(greet); // Hello World!
+alert(PI); // 3.14
+alert(multiplyNumbers(6, 15)); // 90
---
+++ ecmascript/modules/main.js
@@ -0,0 +1,9 @@
+let greet = "Hello World!";
+const PI = 3.14; 
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Exporting variables and functions
+export { greet, PI, multiplyNumbers };
